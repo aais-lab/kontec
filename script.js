17c61a94a41be30aef247187c6904dc6172d6fe6
NaoYamanouchi <nao.yamanouchi@ga.aais-lab.org>
2023-12-05 04:21:45 +0900

    deploy version 2022

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function () {
+	viewSlide('.top-image');
+});
+function viewSlide(className, slideNo = -1)
+{
+	let imgArray = document.querySelectorAll(className);
+	if (slideNo >= 0) {
+		//初回以外は現在のスライドを消す
+		imgArray[slideNo].style.opacity = 0;
+	}
+	slideNo++;
+	if (slideNo >= imgArray.length) {
+		slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
+	}
+	imgArray[slideNo].style.opacity = 1;
+	setTimeout(function(){viewSlide(className, slideNo);}, 5000);
+}
